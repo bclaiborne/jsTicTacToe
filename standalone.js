f8ee6f913ac73ad6e7a6b6828023b2f863fa413e
bclaiborne <bclaiborne@puc.edu>
2015-03-05 22:56:08 -0800

    accept header issues

--- standalone.js
+++ standalone.js
@@ -18,10 +18,11 @@ Server.prototype.start = function(){
 			headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
 			headers["Access-Control-Allow-Credentials"] = false;
 			headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-			headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+//			headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, accept";
 			response.writeHead(200, headers);
 			response.end();
 		} else {
+console.log(response.headers["accept"]);
 			srv.content_type = srv.setResponseType(request);
 			response.writeHead(200, {
 					"Content-Type": srv.content_type,
